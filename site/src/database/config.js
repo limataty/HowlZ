fee6b32a3a7c52bb32f02d663f30e5ee2dd5e668
Gustavo <gustavo.andrade@sptech.school>
2023-11-13 19:22:22 -0300

    Modificação para Testar o SQL Server no Site

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -3,18 +3,15 @@ var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
 var sqlServerConfig = {
-    server: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    server: "32.206.56.159",
+    database: "howlz",
+    user: "sa",
+    password: "urubu100",
     pool: {
         max: 10,
         min: 0,
         idleTimeoutMillis: 30000
     },
-    options: {
-        encrypt: true, // for azure
-    }
 }
 
 // CONEXÃO DO MYSQL WORKBENCH
