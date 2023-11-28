f804968982e0d962c8cb2e51a27cca10242c9d91
Gustavo <gustavo.andrade@sptech.school>
2023-11-28 20:39:18 -0300

    Atualização Model

--- site/src/models/processoModel.js
+++ site/src/models/processoModel.js
@@ -2,36 +2,35 @@ var database = require("../database/config");
 
 function contarProcessoModel(idMaquina) {
     var instrucao = "";
-    
-    if(process.env.AMBIENTE_PROCESSO == "producao"){
+
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
         instrucao = `
         SELECT 
-        COUNT(*) AS TotalLinhas
+            COUNT(*) AS TotalLinhas
         FROM 
-        Processo
-        JOIN MonitoramentoProcesso ON Processo.idProcesso = MonitoramentoProcesso.fkProcesso
-        JOIN UnidadeMedida ON UnidadeMedida.idUnidadeMedida = MonitoramentoProcesso.fkUnidadeMedida
-        JOIN TipoComponente ON TipoComponente.idTipoComponente = MonitoramentoProcesso.fkTipoComponente
+            Processo
+            JOIN MonitoramentoProcesso ON Processo.idProcesso = MonitoramentoProcesso.fkProcesso
+            JOIN UnidadeMedida ON UnidadeMedida.idUnidadeMedida = MonitoramentoProcesso.fkUnidadeMedida
+            JOIN TipoComponente ON TipoComponente.idTipoComponente = MonitoramentoProcesso.fkTipoComponente
         WHERE 
-        MonitoramentoProcesso.dataHora >= DATEADD(DAY, -2, GETDATE()) AND fkComputador = ${idMaquina};
+            MonitoramentoProcesso.dataHora >= DATEADD(DAY, -2, GETDATE()) AND fkComputador = ${idMaquina};
         `;
-    } else if(process.env.AMBIENTE_PROCESSO == "desenvolvimento"){
-    instrucao = `
-    SELECT 
-    COUNT(*) AS TotalLinhas
-    FROM 
-    Processo
-    JOIN MonitoramentoProcesso ON idProcesso = fkProcesso
-    JOIN UnidadeMedida ON idUnidadeMedida = fkUnidadeMedida
-    JOIN TipoComponente ON idTipoComponente = fkTipoComponente
-    WHERE 
-    MonitoramentoProcesso.dataHora >= CURDATE() - INTERVAL 1 WEEK AND fkComputador = ${idMaquina};
-    `;
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucao = `
+        SELECT 
+            COUNT(*) AS TotalLinhas
+        FROM 
+            Processo
+            JOIN MonitoramentoProcesso ON idProcesso = fkProcesso
+            JOIN UnidadeMedida ON idUnidadeMedida = fkUnidadeMedida
+            JOIN TipoComponente ON idTipoComponente = fkTipoComponente
+        WHERE 
+            MonitoramentoProcesso.dataHora >= CURDATE() - INTERVAL 1 WEEK AND fkComputador = ${idMaquina};
+        `;
+    }
 
     console.log(instrucao);
     return database.executar(instrucao);
-    }
-    
 }
 
 // function contarJanela(idMaquina) {
@@ -62,7 +61,7 @@ function contarProcessoModel(idMaquina) {
 function exibirProcesso(idMaquina) {
     var instrucao = "";
 
-    if(process.env.AMBIENTE_PROCESSO == "producao"){
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
         instrucao = `
         WITH RankedMonitoramento AS (
             SELECT TOP 1 
@@ -75,12 +74,9 @@ function exibirProcesso(idMaquina) {
                 ROW_NUMBER() OVER (PARTITION BY Processo.idProcesso ORDER BY MonitoramentoProcesso.dataHora DESC) AS RowNum
             FROM 
                 Processo
-            JOIN 
-                MonitoramentoProcesso ON Processo.idProcesso = MonitoramentoProcesso.fkProcesso
-            JOIN 
-                UnidadeMedida ON UnidadeMedida.idUnidadeMedida = MonitoramentoProcesso.fkUnidadeMedida
-            JOIN 
-                TipoComponente ON TipoComponente.idTipoComponente = MonitoramentoProcesso.fkTipoComponente
+                JOIN MonitoramentoProcesso ON Processo.idProcesso = MonitoramentoProcesso.fkProcesso
+                JOIN UnidadeMedida ON UnidadeMedida.idUnidadeMedida = MonitoramentoProcesso.fkUnidadeMedida
+                JOIN TipoComponente ON TipoComponente.idTipoComponente = MonitoramentoProcesso.fkTipoComponente
             WHERE 
                 Processo.fkComputador = ${idMaquina} AND
                 MonitoramentoProcesso.dataHora >= DATEADD(DAY, -2, GETDATE())
@@ -98,27 +94,28 @@ function exibirProcesso(idMaquina) {
             RowNum = 1
         ORDER BY 
             dataHora DESC;
-        
         `;
-    } else if(process.env.AMBIENTE_PROCESSO == "desenvolvimento"){
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucao = `
-    SELECT
-    Processo.nome AS NomeProcesso,
-    MonitoramentoProcesso.dataHora,
-    fkTipoComponente,
-    TipoComponente.nome as NomeComponente,
-    uso,
-    UnidadeMedida.simbolo
-    FROM
-    Processo
-    JOIN MonitoramentoProcesso ON idProcesso = fkProcesso
-    JOIN UnidadeMedida ON idUnidadeMedida = fkUnidadeMedida
-    JOIN TipoComponente ON idTipoComponente = fkTipoComponente
-    WHERE
-    Processo.fkComputador = ${idMaquina} AND
-    MonitoramentoProcesso.dataHora >= CURDATE() - INTERVAL 1 WEEK
-    LIMIT 1;
-    `;
+        SELECT
+            Processo.nome AS NomeProcesso,
+            MonitoramentoProcesso.dataHora,
+            fkTipoComponente,
+            TipoComponente.nome as NomeComponente,
+            uso,
+            UnidadeMedida.simbolo
+        FROM
+            Processo
+            JOIN MonitoramentoProcesso ON idProcesso = fkProcesso
+            JOIN UnidadeMedida ON idUnidadeMedida = fkUnidadeMedida
+            JOIN TipoComponente ON idTipoComponente = fkTipoComponente
+        WHERE
+            Processo.fkComputador = ${idMaquina} AND
+            MonitoramentoProcesso.dataHora >= CURDATE() - INTERVAL 1 WEEK
+        ORDER BY 
+            MonitoramentoProcesso.dataHora DESC
+        LIMIT 1;
+        `;
     }
 
     console.log(instrucao);
