09a30352147e154ef9501d1e6bfb3169978f5162
Gustavo <gustavo.andrade@sptech.school>
2023-11-28 13:18:34 -0300

    Atualização Model

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -239,6 +239,7 @@ function alertas(fkEmpresa){
     WHERE
         fkEmpresa = ${fkEmpresa};
     `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
 
