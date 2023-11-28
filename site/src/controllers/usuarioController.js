c6a009906f6aabbcfd443f0aa8ccd395c0c9a0e2
Gustavo <gustavo.andrade@sptech.school>
2023-11-28 13:17:33 -0300

    Atualização Model

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -450,7 +450,7 @@ function alertas(req, res){
     })
     .catch(function (erro) {
       console.log(erro);
-      console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
+      console.log("Houve um erro ao realizar o alerta: ", erro.sqlMessage);
       res.status(500).json(erro.sqlMessage);
     });
 }
